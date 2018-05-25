459f8a2b5dbb5e3c65024dc6f7ca721d1ae158ff
ripper66 <amrirazlan@gmail.com>
2018-05-25 11:18:56 +0800

    Update mlwr_tst.py

--- mlwr_tst.py
+++ mlwr_tst.py
@@ -2,7 +2,6 @@
 # This script will run a HTTP Request, please use Python 3 to run it.
 
 import urllib.request
-#header="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36
 
 print("""\
 ______  ___      ______                                  ________           _____ 
